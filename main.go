aafcdaed3218deab31274d2aaa96c3352cb26994
Don <don.j.olmstead@gmail.com>
2016-05-25 12:15:01 -0700

    Update to 0.5

--- main.go
+++ main.go
@@ -1,243 +1,75 @@
 package main
 
 import (
-	"encoding/base64"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"os"
-	"os/exec"
-	"path"
 	"strings"
 
-	"github.com/drone/drone-go/drone"
-	"github.com/drone/drone-go/plugin"
+	"github.com/codegangsta/cli"
+	_ "github.com/joho/godotenv/autoload"
 )
 
-type Npm struct {
-	Username   string `json:"username"`
-	Password   string `json:"password"`
-	Email      string `json:"email"`
-	Registry   string `json:"registry"`
-	Folder     string `json:"folder"`
-	AlwaysAuth bool   `json:"always_auth"`
-}
-
-type NpmPackage struct {
-	Name    string `json:"name"`
-	Version string `json:"version"`
-}
-
-var (
-	buildCommit string
-)
+var version string // build number set at compile-time
 
 func main() {
-	fmt.Printf("Drone NPM Plugin built from %s\n", buildCommit)
+	app := cli.NewApp()
+	app.Name = "npm"
+	app.Usage = "npm plugin"
+	app.Action = run
+	app.Version = version
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "username",
+			Usage:  "NPM username",
+			EnvVar: "PLUGIN_USERNAME,NPM_USERNAME",
+		},
+		cli.StringFlag{
+			Name:   "password",
+			Usage:  "NPM password",
+			EnvVar: "PLUGIN_PASSWORD,NPM_PASSWORD",
+		},
+		cli.StringFlag{
+			Name:   "email",
+			Usage:  "NPM email",
+			EnvVar: "PLUGIN_EMAIL,NPM_EMAIL",
+		},
+		cli.StringFlag{
+			Name:   "registry",
+			Usage:  "NPM registry",
+			Value:  GlobalRegistry,
+			EnvVar: "PLUGIN_REGISTRY,NPM_REGISTRY",
+		},
+		cli.StringFlag{
+			Name:   "folder",
+			Usage:  "folder containing package.json",
+			EnvVar: "PLUGIN_FOLDER",
+		},
+		cli.BoolFlag{
+			Name:   "always_auth",
+			Usage:  "always authorize",
+			EnvVar: "PLUGIN_ALWAYS_AUTH,NPM_ALWAYS_AUTH",
+		},
+	}
 
-	repo := drone.Repo{}
-	build := drone.Build{}
-	workspace := drone.Workspace{}
-	vargs := Npm{}
+	app.Run(os.Args)
+}
 
-	plugin.Param("build", &build)
-	plugin.Param("repo", &repo)
-	plugin.Param("workspace", &workspace)
-	plugin.Param("vargs", &vargs)
+func run(c *cli.Context) error {
+	plugin := Plugin{
+		Config: Config{
+			Username:   c.String("username"),
+			Password:   c.String("password"),
+			Email:      c.String("email"),
+			Registry:   c.String("registry"),
+			Folder:     c.String("folder"),
+			AlwaysAuth: c.Bool("always_auth"),
+		},
+	}
 
-	// parse the parameters
-	if err := plugin.Parse(); err != nil {
-		fmt.Println(err.Error())
+	if err := plugin.Exec(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// check for required parameters
-	if len(vargs.Username) == 0 {
-		fmt.Println("Username not provided")
-		os.Exit(1)
-	}
-
-	if len(vargs.Password) == 0 {
-		fmt.Println("Password not provided")
-		os.Exit(1)
-	}
-
-	if len(vargs.Email) == 0 {
-		fmt.Println("Email not provided")
-		os.Exit(1)
-	}
-
-	// set defaults
-	var globalRegistry bool
-
-	if len(vargs.Registry) == 0 {
-		vargs.Registry = "https://registry.npmjs.org"
-		globalRegistry = true
-	} else {
-		globalRegistry = false
-	}
-
-	// get the package info
-	var packagePath string
-
-	if len(vargs.Folder) == 0 {
-		packagePath = path.Join(workspace.Path)
-	} else {
-		packagePath = path.Join(workspace.Path, vargs.Folder)
-	}
-
-	packageFile := path.Join(packagePath, "package.json")
-
-	npmPackage, err := readPackageFile(packageFile)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-
-	// see if the package should be published
-	publish, err := shouldPublishPackage(vargs, npmPackage)
-
-	if publish {
-		fmt.Println("Attempting to publish package")
-
-		// write the npmrc file
-		err := writeNpmrcFile(vargs)
-
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
-
-		var cmds []*exec.Cmd
-
-		// write registry command
-		if !globalRegistry {
-			cmds = append(cmds, registryCommand(vargs))
-		}
-
-		// write auth command
-		if vargs.AlwaysAuth {
-			cmds = append(cmds, alwaysAuthCommand())
-		}
-
-		// write the publish command
-		cmds = append(cmds, publishCommand())
-
-		// run the commands
-		for _, cmd := range cmds {
-			cmd.Dir = packagePath
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			trace(cmd)
-			err := cmd.Run()
-			if err != nil {
-				os.Exit(1)
-			}
-		}
-	} else {
-		fmt.Println("Package already published")
-	}
-}
-
-/// Reads the package file at the given path
-func readPackageFile(path string) (*NpmPackage, error) {
-	// read the file
-	file, err := ioutil.ReadFile(path)
-
-	if err != nil {
-		return nil, err
-	}
-
-	// unmarshal the json data
-	npmPackage := NpmPackage{}
-	err = json.Unmarshal(file, &npmPackage)
-
-	if err != nil {
-		return nil, err
-	}
-
-	// make sure values are present
-	if len(npmPackage.Name) == 0 {
-		return nil, errors.New("No package name present")
-	}
-
-	if len(npmPackage.Version) == 0 {
-		return nil, errors.New("No package version present")
-	}
-
-	return &npmPackage, nil
-}
-
-/// Determines if the package should be published
-func shouldPublishPackage(vargs Npm, npmPackage *NpmPackage) (bool, error) {
-	// get the url for the package
-	packageUrl := fmt.Sprintf("%s/%s/%s", vargs.Registry, npmPackage.Name, npmPackage.Version)
-	fmt.Printf("Requesting %s\n", packageUrl)
-
-	// create a request for the package
-	req, err := http.NewRequest("GET", packageUrl, nil)
-
-	if err != nil {
-		return false, err
-	}
-
-	// set authentication if needed
-	// NPM uses basic http auth
-	if vargs.AlwaysAuth {
-		req.SetBasicAuth(vargs.Username, vargs.Password)
-	}
-
-	// get the response
-	resp, err := http.DefaultClient.Do(req)
-
-	if err != nil {
-		return false, err
-	}
-
-	defer resp.Body.Close()
-
-	// look for a 404 to see if the package should be published
-	if resp.StatusCode != http.StatusNotFound {
-		return false, nil
-	} else {
-		return true, nil
-	}
-}
-
-/// Writes the npmrc file
-func writeNpmrcFile(vargs Npm) error {
-
-	// get the base64 encoded string
-	authString := fmt.Sprintf("%s:%s", vargs.Username, vargs.Password)
-	encoded := base64.StdEncoding.EncodeToString([]byte(authString))
-
-	// create the file contents
-	contents := fmt.Sprintf("_auth = %s\nemail = %s", encoded, vargs.Email)
-
-	// write the file
-	return ioutil.WriteFile("/root/.npmrc", []byte(contents), 0644)
-}
-
-// Sets the npm registry
-func registryCommand(vargs Npm) *exec.Cmd {
-	return exec.Command("npm", "config", "set", "registry", vargs.Registry)
-}
-
-// Sets the always off flag
-func alwaysAuthCommand() *exec.Cmd {
-	return exec.Command("npm", "config", "set", "always-auth", "true")
-}
-
-// Publishes the package
-func publishCommand() *exec.Cmd {
-	return exec.Command("npm", "publish")
-}
-
-// Trace writes each command to standard error (preceded by a ‘$ ’) before it
-// is executed. Used for debugging your build.
-func trace(cmd *exec.Cmd) {
-	fmt.Println("$", strings.Join(cmd.Args, " "))
+	return nil
 }
---
+++ plugin.go
@@ -0,0 +1,235 @@
+package main
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+type (
+	Config struct {
+		Username   string
+		Password   string
+		Email      string
+		Registry   string
+		Folder     string
+		AlwaysAuth bool
+	}
+
+	NpmPackage struct {
+		Name    string `json:"name"`
+		Version string `json:"version"`
+	}
+
+	Plugin struct {
+		Config Config
+	}
+)
+
+const GlobalRegistry = "https://registry.npmjs.org"
+
+func (p Plugin) Exec() error {
+	// read the package
+	packagePath := path.Join(p.Config.Folder, "package.json")
+
+	npmPackage, err := readPackageFile(packagePath)
+
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Error("Could not read package.json")
+		return err
+	}
+
+	log.WithFields(log.Fields{
+		"name":    npmPackage.Name,
+		"version": npmPackage.Version,
+	}).Info("Found package")
+
+	// see if the package should be published
+	publish, err := shouldPublishPackage(p.Config, npmPackage)
+
+	if publish {
+		log.Info("Attempting to publish package")
+
+		// write the npmrc file
+		err := writeNpmrcFile(p.Config)
+
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error": err,
+			}).Error("Problem creating npmrc file")
+
+			return err
+		}
+
+		var cmds []*exec.Cmd
+
+		// write registry command
+		if p.Config.Registry != GlobalRegistry {
+			cmds = append(cmds, registryCommand(p.Config.Registry))
+		}
+
+		// write auth command
+		if p.Config.AlwaysAuth {
+			cmds = append(cmds, alwaysAuthCommand())
+		}
+
+		// write the publish command
+		cmds = append(cmds, publishCommand())
+
+		// run the commands
+		err = runCommands(cmds)
+
+		if err != nil {
+			log.WithFields(log.Fields{
+				"error": err,
+			}).Error("Package was not published")
+		}
+	} else {
+		log.WithFields(log.Fields{
+			"reason": err,
+		}).Info("Package will not be published")
+	}
+
+	return nil
+}
+
+/// Reads the package file at the given path
+func readPackageFile(path string) (*NpmPackage, error) {
+	// read the file
+	file, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// unmarshal the json data
+	npmPackage := NpmPackage{}
+	err = json.Unmarshal(file, &npmPackage)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// make sure values are present
+	if len(npmPackage.Name) == 0 {
+		return nil, errors.New("No package name present")
+	}
+
+	if len(npmPackage.Version) == 0 {
+		return nil, errors.New("No package version present")
+	}
+
+	return &npmPackage, nil
+}
+
+/// Determines if the package should be published
+func shouldPublishPackage(config Config, npmPackage *NpmPackage) (bool, error) {
+	// get the url for the package
+	//
+	// encoding the portion of the url for the package name in case of scopes
+	packageUrl := fmt.Sprintf("%s/%s/%s", config.Registry, url.QueryEscape(npmPackage.Name), npmPackage.Version)
+
+	log.WithFields(log.Fields{
+		"url": packageUrl,
+	}).Info("Requesting package information")
+
+	// create a request for the package
+	req, err := http.NewRequest("GET", packageUrl, nil)
+
+	if err != nil {
+		return false, err
+	}
+
+	// set authentication if needed
+	// NPM uses basic http auth
+	if config.AlwaysAuth {
+		req.SetBasicAuth(config.Username, config.Password)
+	}
+
+	// get the response
+	resp, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		return false, err
+	}
+
+	defer resp.Body.Close()
+
+  statusCode := resp.StatusCode
+
+	// look for a 404 to see if the package should be published
+	if statusCode == http.StatusNotFound {
+    return true, nil
+  } else if statusCode == http.StatusOK {
+		return false, errors.New("Package already published")
+	} else {
+    contents, err := ioutil.ReadAll(resp.Body)
+
+    if err != nil {
+      return false, err
+    }
+
+		return false, errors.New(fmt.Sprintf("Error Occurred. Status %d\nBody:%s", statusCode, contents))
+	}
+}
+
+/// Writes the npmrc file
+func writeNpmrcFile(config Config) error {
+	// get the base64 encoded string
+	authString := fmt.Sprintf("%s:%s", config.Username, config.Password)
+	encoded := base64.StdEncoding.EncodeToString([]byte(authString))
+
+	// create the file contents
+	contents := fmt.Sprintf("_auth = %s\nemail = %s", encoded, config.Email)
+
+	// write the file
+	return ioutil.WriteFile("/root/.npmrc", []byte(contents), 0644)
+}
+
+// Sets the npm registry
+func registryCommand(registry string) *exec.Cmd {
+	return exec.Command("npm", "config", "set", "registry", registry)
+}
+
+// Sets the always off flag
+func alwaysAuthCommand() *exec.Cmd {
+	return exec.Command("npm", "config", "set", "always-auth", "true")
+}
+
+// Publishes the package
+func publishCommand() *exec.Cmd {
+	return exec.Command("npm", "publish")
+}
+
+// Trace writes each command to standard error (preceded by a ‘$ ’) before it
+// is executed. Used for debugging your build.
+func trace(cmd *exec.Cmd) {
+	fmt.Fprintf(os.Stdout, "+ %s\n", strings.Join(cmd.Args, " "))
+}
+
+func runCommands(cmds []*exec.Cmd) error {
+	for _, cmd := range cmds {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		trace(cmd)
+
+		err := cmd.Run()
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
