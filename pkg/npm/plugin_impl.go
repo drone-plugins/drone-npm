81ecb40fa2f2570d362154eb3bceb97d0575b35f
Don <don.j.olmstead@gmail.com>
2019-12-18 15:46:25 -0800

    Migrate plugin

--- pkg/npm/plugin_impl.go
+++ pkg/npm/plugin_impl.go
@@ -6,10 +6,14 @@
 package npm
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/url"
 	"os"
+	"os/exec"
+	"os/user"
 	"path"
 	"strings"
 
@@ -80,12 +84,147 @@ func (p *pluginImpl) Validate() error {
 		return fmt.Errorf("Registry values do not match .drone.yml: %s package.json: %s", p.settings.Registry, npm.Config.Registry)
 	}
 
+	p.npm = npm
+
 	return nil
 }
 
 func (p *pluginImpl) Exec() error {
-	// Implementation of the plugin.
+	// Write the npmrc file
+	if err := p.writeNpmrc(); err != nil {
+		return fmt.Errorf("Could not create npmrc %w", err)
+	}
+
+	// Attempt authentication
+	if err := p.authenticate(); err != nil {
+		return fmt.Errorf("Could not authenticate %w", err)
+	}
+
+	// Determine whether to publish
+	publish, err := p.shouldPublishPackage()
+
+	if err != nil {
+		return fmt.Errorf("Could not determine if package should be published %w", err)
+	}
+
+	if publish {
+		logrus.Info("Publishing package")
+		if err = runCommand(publishCommand(p.settings), p.settings.Folder); err != nil {
+			return fmt.Errorf("Could not publish package %w", err)
+		}
+	} else {
+		logrus.Info("Not publishing package")
+	}
+
 	return nil
+}
+
+/// writeNpmrc creates a .npmrc in the folder for authentication
+func (p *pluginImpl) writeNpmrc() error {
+	var f func(settings Settings) string
+    if len(p.settings.Token) == 0 {
+		logrus.WithFields(logrus.Fields{
+			"username": p.settings.Username,
+			"email":    p.settings.Email,
+		}).Info("Specified credentials")
+        f = npmrcContentsUsernamePassword
+	} else {
+		logrus.Info("Token credentials being used")
+		f = npmrcContentsToken
+	}
+
+	// write npmrc file
+	home := "/root"
+	user, err := user.Current()
+	if err == nil {
+		home = user.HomeDir
+	}
+	npmrcPath := path.Join(home, ".npmrc")
+
+	logrus.WithField("path", npmrcPath).Info("Writing npmrc")
+
+	return ioutil.WriteFile(npmrcPath, []byte(f(p.settings)), 0644)
+}
+
+/// authenticate atempts to authenticate with the NPM registry.
+func (p *pluginImpl) authenticate() error {
+	var cmds []*exec.Cmd
+
+	// Write the version command
+	cmds = append(cmds, versionCommand())
+
+	// write registry command
+	if p.settings.Registry != globalRegistry {
+		cmds = append(cmds, registryCommand(p.settings.Registry))
+	}
+
+	// Write auth command
+	cmds = append(cmds, alwaysAuthCommand())
+
+	// Write skip verify command
+	if p.network.SkipVerify {
+		cmds = append(cmds, skipVerifyCommand())
+	}
+
+	// Write whoami command to verify credentials
+	cmds = append(cmds, whoamiCommand())
+
+	// Run commands
+	err := runCommands(cmds, p.settings.Folder)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+/// shouldPublishPackage determines if the package should be published
+func (p *pluginImpl) shouldPublishPackage() (bool, error) {
+	cmd := packageVersionsCommand(p.npm.Name)
+	cmd.Dir = p.settings.Folder
+
+	trace(cmd)
+	out, err := cmd.CombinedOutput()
+
+	// see if there was an error
+	// if there is an error its likely due to the package never being published
+	if err == nil {
+		// parse the json output
+		var versions []string
+		err = json.Unmarshal(out, &versions)
+
+		if err != nil {
+			logrus.Debug("Could not parse into array of string. Likely single value")
+
+			var version string
+			err := json.Unmarshal(out, &version)
+
+			if err != nil {
+				return false, err
+			}
+
+			versions = append(versions, version)
+		}
+
+		for _, value := range versions {
+			logrus.WithField("version", value).Debug("Found version of package")
+
+			if strings.Compare(p.npm.Version, value) == 0 {
+				logrus.Info("Version found in the registry")
+				if p.settings.FailOnVersionConflict {
+					return false, fmt.Errorf("Cannot publish package due to version conflict")
+				}
+				return false, nil
+			}
+		}
+
+		logrus.Info("Version not found in the registry")
+	} else {
+		logrus.Info("Name was not found in the registry")
+	}
+
+	return true, nil
 }
 
 /// readPackageFile reads the package file at the given path.
@@ -136,4 +275,94 @@ func readPackageFile(folder string) (*npmPackage, error) {
 	}).Info("Found package.json")
 
 	return &npm, nil
+}
+
+// npmrcContentsUsernamePassword creates the contents from a username and
+// password
+func npmrcContentsUsernamePassword(settings Settings) string {
+	// get the base64 encoded string
+	authString := fmt.Sprintf("%s:%s", settings.Username, settings.Password)
+	encoded := base64.StdEncoding.EncodeToString([]byte(authString))
+
+	// create the file contents
+	return fmt.Sprintf("_auth = %s\nemail = %s", encoded, settings.Email)
+}
+
+/// Writes npmrc contents when using a token
+func npmrcContentsToken(settings Settings) string {
+	registry, _ := url.Parse(settings.Registry)
+	return fmt.Sprintf("//%s/:_authToken=%s", registry.Host, settings.Token)
+}
+
+// versionCommand gets the npm version
+func versionCommand() *exec.Cmd {
+	return exec.Command("npm", "--version")
+}
+
+// registryCommand sets the NPM registry.
+func registryCommand(registry string) *exec.Cmd {
+	return exec.Command("npm", "config", "set", "registry", registry)
+}
+
+// alwaysAuthCommand forces authentication.
+func alwaysAuthCommand() *exec.Cmd {
+	return exec.Command("npm", "config", "set", "always-auth", "true")
+}
+
+// skipVerifyCommand disables ssl verification.
+func skipVerifyCommand() *exec.Cmd {
+	return exec.Command("npm", "config", "set", "strict-ssl", "false")
+}
+
+// whoamiCommand creates a command that gets the currently logged in user.
+func whoamiCommand() *exec.Cmd {
+	return exec.Command("npm", "whoami")
+}
+
+// packageVersionsCommand gets the versions of the npm package.
+func packageVersionsCommand(name string) *exec.Cmd {
+	return exec.Command("npm", "view", name, "versions", "--json")
+}
+
+// publishCommand runs the publish command
+func publishCommand(settings Settings) *exec.Cmd {
+	commandArgs := []string{"publish"}
+
+	if len(settings.Tag) != 0 {
+		commandArgs = append(commandArgs, "--tag", settings.Tag)
+	}
+
+	if len(settings.Access) != 0 {
+		commandArgs = append(commandArgs, "--access", settings.Access)
+	}
+
+	return exec.Command("npm", commandArgs...)
+}
+
+// trace writes each command to standard error (preceded by a ‘$ ’) before it
+// is executed. Used for debugging your build.
+func trace(cmd *exec.Cmd) {
+	fmt.Fprintf(os.Stdout, "+ %s\n", strings.Join(cmd.Args, " "))
+}
+
+// runCommands executes the list of cmds in the given directory.
+func runCommands(cmds []*exec.Cmd, dir string) error {
+	for _, cmd := range cmds {
+		err := runCommand(cmd, dir)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func runCommand(cmd *exec.Cmd, dir string) error {
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Dir = dir
+	trace(cmd)
+
+	return cmd.Run()
 }
